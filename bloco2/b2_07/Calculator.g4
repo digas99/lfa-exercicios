96f5daac77900b613da00f16489d97bfa599805d
Diogo <diogo.correia99@ua.pt>
2020-04-23 19:13:08 +0100

    added exercise 2.7. Updated README

---
+++ bloco2/b2_07/Calculator.g4
@@ -0,0 +1,24 @@
+grammar Calculator;
+
+program     :   stat* EOF ;
+
+stat        :   expr NEWLINE                # StatExpr
+            |   assign NEWLINE              # StatAssign
+            |   NEWLINE                     # StatNewLine
+            ;
+
+assign      :   ID '=' expr ;
+
+expr        :   op=('-'|'+') expr           # ExprBeginsWith
+            |   expr op=('*'|'/'|'%') expr  # ExprMultDivMod
+            |   expr op=('+'|'-') expr      # ExprAddSub
+            |   Integer                     # ExprInteger
+            |   '(' expr ')'                # ExprParens
+            |   ID                          # ExprId
+            ;
+        
+Integer :   [0-9]+ ;
+ID      :   [a-zA-Z_]+ ;
+NEWLINE :   '\r'? '\n' ;
+COMMENT :   '#' .*? '\n' -> skip ;
+WS  :   [ \t]+ -> skip ;
---
+++ bloco2/b2_07/MyVisitor.java
@@ -0,0 +1,59 @@
+import java.util.Stack;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+
+public class MyVisitor extends CalculatorBaseVisitor<String> {
+
+   static Stack<String> ops = new Stack<>();
+   static HashMap<String, Integer> variables = new HashMap<>();
+
+   @Override public String visitProgram(CalculatorParser.ProgramContext ctx) {
+      for (CalculatorParser.StatContext s : ctx.stat()) {
+         if (visit(s) != null)
+            System.out.println(visit(s));
+      }
+      return null;
+   }
+
+   @Override public String visitStatExpr(CalculatorParser.StatExprContext ctx) {
+      String exp = visit(ctx.expr());
+      // System.out.println(exp);
+      return exp;
+   }
+   
+   @Override public String visitStatAssign(CalculatorParser.StatAssignContext ctx) {
+      return visit(ctx.assign());
+   }
+
+   @Override public String visitAssign(CalculatorParser.AssignContext ctx) {
+      return ctx.ID().getText() + " = " + visit(ctx.expr());
+   }
+
+   @Override public String visitExprAddSub(CalculatorParser.ExprAddSubContext ctx) {
+      //ops.push(ctx.op.getText());
+      String exp = visit(ctx.expr(0)) + " " + visit(ctx.expr(1)) + " " + ctx.op.getText();
+      // System.out.print(exp);
+      return exp;
+   }
+
+   @Override public String visitExprParens(CalculatorParser.ExprParensContext ctx) {
+      return visit(ctx.expr());
+   }
+
+   @Override public String visitExprInteger(CalculatorParser.ExprIntegerContext ctx) {
+      return ctx.Integer().getText();
+   }
+
+   @Override public String visitExprBeginsWith(CalculatorParser.ExprBeginsWithContext ctx) {
+      return "0 " + visit(ctx.expr()) + " " + ctx.op.getText();
+   }
+
+   @Override public String visitExprMultDivMod(CalculatorParser.ExprMultDivModContext ctx) {
+      return visit(ctx.expr(0)) + " " + visit(ctx.expr(1)) + " " + ctx.op.getText();
+   }
+
+   @Override public String visitExprId(CalculatorParser.ExprIdContext ctx) {
+      return ctx.ID().getText();
+   }
+}
